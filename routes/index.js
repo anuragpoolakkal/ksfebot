a62fe5fb41751df8b6cde07f7e5965f3159ace15
Anurag <anuragrajanp@gmail.com>
2023-11-09 18:45:53 +0530

    vercel config

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,8 @@
 import express from "express";
-import WhatsappCloudAPI from "whatsappcloudapi_wrapper";
 import "dotenv/config";
 import handleMessages from "../services/handleMessages.js";
 
 const router = express.Router();
-
-const Whatsapp = new WhatsappCloudAPI({
-	accessToken: process.env.Meta_WA_accessToken,
-	senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-	WABA_ID: process.env.Meta_WA_wabaId,
-});
 
 router.get("/endpoint", (req, res) => {
 	try {
