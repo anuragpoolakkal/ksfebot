a4d6cd109e793e4242306038726281622c979554
Anvin.C.Shaju <anvincsg@gmail.com>
2023-11-10 11:01:42 +0530

    test faq

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const faqList = [
+    {
+        id: "q1",
+        question: "Question 1?",
+        answer: "Answer 1.",
+    },
+    {
+        id: "q2",
+        question: "Question 2?",
+        answer: "Answer 2.",
+    },
+    {
+        id: "q3",
+        question: "Question 3?",
+        answer: "Answer 3.",
+    },
+    {
+        id: "q4",
+        question: "Question 4?",
+        answer: "Answer 4.",
+    },
+];
